6294e33e291b8603ade97d2bf9a5ff0ebc3461fc
山﨑 一読 <kazuyoshi.yamasaki@QL201-Macbookpro-N.local>
2021-05-29 16:44:59 +0900

    [add]state追加

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import reactDom from 'react-dom';
+//import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
@@ -57,15 +57,53 @@ import reportWebVitals from './reportWebVitals';
 
 // setInterval(tick, 1000)
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-const element = <Welcome name="Sara" />;
 ReactDOM.render(
-  element,
+  <Clock />,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
