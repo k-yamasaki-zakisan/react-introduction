981ccaf042ce0a761e89d6cd620ad508a30f1ed2
山﨑 一読 <kazuyoshi.yamasaki@QL201-Macbookpro-N.local>
2021-05-30 17:23:38 +0900

    [add]継承その二

--- src/index.js
+++ src/index.js
@@ -331,6 +331,67 @@ import reportWebVitals from './reportWebVitals';
 //   document.getElementById('root')
 // );
 
+// function FancyBorder(props) {
+//   return (
+//     <div className={'FancyBorder FancyBorder-' + props.color}>
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// function WelcomeDialog() {
+//   return (
+//     <FancyBorder color="blue">
+//       <h1 className="Dialog-title">
+//         Welcome
+//       </h1>
+//       <p className="Dialog-message">
+//         Thank you for visiting our spacecraft!
+//       </p>
+//     </FancyBorder>
+//   );
+// }
+
+// ReactDOM.render(
+//   <WelcomeDialog />,
+//   document.getElementById('root')
+// );
+
+// function FancyBorder(props) {
+//   return (
+//     <div className={'FancyBorder FancyBorder-' + props.color}>
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// function Dialog(props) {
+//   return (
+//     <FancyBorder color="blue">
+//       <h1 className="Dialog-title">
+//         {props.title}
+//       </h1>
+//       <p className="Dialog-message">
+//         {props.message}
+//       </p>
+//       {props.children}
+//     </FancyBorder>
+//   );
+// }
+
+// function WelcomeDialog() {
+//   return (
+//     <Dialog
+//       title="Welcome"
+//       message="Thank you for visiting our spacecraft!" />
+//   );
+// }
+
+// ReactDOM.render(
+//   <WelcomeDialog />,
+//   document.getElementById('root')
+// );
+
 function FancyBorder(props) {
   return (
     <div className={'FancyBorder FancyBorder-' + props.color}>
@@ -339,21 +400,52 @@ function FancyBorder(props) {
   );
 }
 
-function WelcomeDialog() {
+function Dialog(props) {
   return (
     <FancyBorder color="blue">
       <h1 className="Dialog-title">
-        Welcome
+        {props.title}
       </h1>
       <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
+        {props.message}
       </p>
+      {props.children}
     </FancyBorder>
   );
 }
 
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlechange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+          onChange={this.handlechange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
 ReactDOM.render(
-  <WelcomeDialog />,
+  <SignUpDialog />,
   document.getElementById('root')
 );
 
